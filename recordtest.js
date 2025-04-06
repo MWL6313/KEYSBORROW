09e52c89f2539e0f83390e7c0e83949275a6b1d8
Ming Wei Liu <n000165242@outlook.com>
2025-04-06 15:06:33 +0800

    Update recordtest.js

--- recordtest.js
+++ recordtest.js
@@ -2,7 +2,7 @@ const token = localStorage.getItem("authToken");
 if (!token) location.href = "index.html";
 
 // Flask API base URL
-const API_BASE = "/api";
+const API_BASE = "http://127.0.0.1:5000/api";
 
 let allRecords = [];
 let currentRole = "";
