9e18916b17bffcbf4b34ec42c633c2a85edd41e0
Ming Wei Liu <n000165242@outlook.com>
2025-08-26 22:33:21 +0800

    Update record.js

--- record.js
+++ record.js
@@ -988,8 +988,8 @@ async function reloadWithTimestamp() {
 // 手動刷新按鈕
 document.getElementById("refreshBtn").addEventListener("click", reloadWithTimestamp);
 
-// 自動每 900 秒 15分更新
-setInterval(reloadWithTimestamp, 900 * 1000);
+// 自動每 120 秒 2分更新
+setInterval(reloadWithTimestamp, 120 * 1000);
 
 
 // 初次載入
